4db9356b4fd1353dbe3ec81a1e7b8efb495dfcfd
nicagali <m.conte@uu.nl>
2026-06-04 15:01:09 +0200

    made figure for msd varying velocity

--- scripts/figures/msd_varv.py
+++ scripts/figures/msd_varv.py
@@ -6,28 +6,39 @@ sys.path.append("src/")
 import plot
 from afbm import afbm
 
-alpha = 0.9
-T = 100
-h = 0.001
-v = 200
-mu = 1
+alpha=0.9
+T=200.
+h=0.001
+mu=1.
+realizations=500
 B_T = 1
 B_R = 1
 KBT = 1
-realizations = 100
-
-DATA_PATH = f"data/msd_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.npz"
-    
-fig, ax = plt.subplots(figsize=(12, 9))
-eq = afbm(alpha, T, h, v, mu, B_T, B_R, KBT)
-plot.plot_msd(ax=ax,eq=eq, component="r", data_path=DATA_PATH)
-ax.set_xscale("log")    
-ax.set_yscale("log")
-plt.savefig(f"plots/rmsd_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.png", dpi=200)
 
 fig, ax = plt.subplots(figsize=(12, 9))
-eq = afbm(alpha, T, h, v, mu, B_T, B_R, KBT)
-plot.plot_msd(ax=ax,eq=eq, component="phi", data_path=DATA_PATH)
-ax.set_xscale("log")    
-ax.set_yscale("log")
-plt.savefig(f"plots/phimsd_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.png", dpi=200)
+
+add_analytics = True
+# for v in [0., 10., 50., 100., 200., 500.]:
+#     DATA_PATH = f"data/msd_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.npz"
+#     eq = afbm(alpha, T, h, v, mu, B_T, B_R, KBT)
+#     plot.plot_msd(ax=ax,eq=eq, component="r", add_analytics=add_analytics, label=fr"$v={eq.v}$", data_path=DATA_PATH)
+#     ax.set_xscale("log")    
+#     ax.set_yscale("log")
+
+# plt.savefig(f"plots/rmsdvarv_a{alpha}_T{T}_h{h}_mu{mu}_r{realizations}.png", dpi=200)
+
+fig, ax = plt.subplots(figsize=(12, 9))
+for v in [0., 10., 50., 100., 200., 500.]:
+
+    DATA_PATH = f"data/msd_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.npz"
+    eq = afbm(alpha, T, h, v, mu, B_T, B_R, KBT)
+    plot.plot_msd(ax=ax,eq=eq, component="phi", add_analytics=add_analytics, label=fr"$v={eq.v}$", data_path=DATA_PATH)
+
+    ax.set_xscale("log")    
+    ax.set_yscale("log")
+
+plot.add_trend(ax, tmin=1, tmax=200, exponent=0.9, yref=1, tref=1, color='blue', linestyle='--')
+plot.add_trend(ax, tmin=0.03, tmax=0.5, exponent=2.9, yref=5e3, tref=1, color='#ff0080', linestyle='--')
+plot.add_trend(ax, tmin=0.01, tmax=0.1, exponent=3.5, yref=5e5, tref=1, color='#bedf1d', linestyle='--')
+ax.legend()                      
+plt.savefig(f"plots/phimsdvarv_a{alpha}_T{T}_h{h}_mu{mu}_r{realizations}.png", dpi=200)
