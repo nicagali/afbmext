01c0ffe71bac22d9f67bc033f94d7a6e1a340a22
nicagali <m.conte@uu.nl>
2026-07-09 10:30:07 +0200

    added ovito writing of traj

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def plot_msd(ax, eq, component, add_analytics, label, data_path):
+def plot_msd(ax, eq, component, add_analytics, label, data_path, ylabel = True, xlabel = True):
 
     # simulation data
     data = np.load(data_path)
@@ -15,10 +15,11 @@ def plot_msd(ax, eq, component, add_analytics, label, data_path):
     ax.plot(t, msd_sim, label=label)
     if add_analytics:
         ax.plot(t_analytical, msd_an, color = 'black', linestyle="--")
-    ax.set_xlabel("t")
-
-    ylabel = r'$\langle (\boldsymbol{{r}}(t)-\boldsymbol{{r}}_0)^2 \rangle$' if component == "r" else r'$\langle (\phi(t)-\phi_0)^2 \rangle$'
-    ax.set_ylabel(ylabel)
+    if xlabel:
+        ax.set_xlabel(r"$t/\tau_R$")
+    if ylabel:
+        ylabel = r'$\langle (\boldsymbol{{r}}(t)-\boldsymbol{{r}}_0)^2 \rangle/a^2$' if component == "r" else r'$\langle (\phi(t)-\phi_0)^2 \rangle$'
+        ax.set_ylabel(ylabel)
     ax.legend()
 
 def add_trend(ax, tmin, tmax, exponent, yref=1, tref=1, **kwargs):
@@ -30,3 +31,21 @@ def add_trend(ax, tmin, tmax, exponent, yref=1, tref=1, **kwargs):
     ax.plot(t, y, label=rf'$~t^{{{exponent}}}$', **kwargs)
 
 
+def npy_to_xyz(filename, r, L):
+    n = r.shape[1]
+
+    with open(filename, "w") as f:
+        for t in range(n):
+            # number of atoms
+            f.write("1\n")
+
+            # comment / metadata line
+            f.write(
+                f'Lattice="{L} 0 0 0 {L} 0 0 0 1.0" '
+                f'Properties=species:S:1:pos:R:3 '
+                f'Frame={t}\n'
+            )
+
+            x = r[0, t]
+            y = r[1, t]
+            f.write(f"A {x} {y} 0.0\n")
