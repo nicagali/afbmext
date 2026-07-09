bd3cd8a92374acecea9762a03b8b9f2aabe9aec2
nicagali <m.conte@uu.nl>
2026-07-09 10:29:17 +0200

    normalized labels traj figure

--- scripts/traj.py
+++ scripts/traj.py
@@ -4,24 +4,46 @@ sys.path.append("src/")
 from afbm import afbm
 import numpy as np
 
-alpha = 0.8
-T = 2
-h = 0.001
-v = 200
-mu = 1
+alpha = 0.95
 
-sim = afbm(alpha=alpha, T=T, h=h, v=v, mu=mu, B_T=1.0, B_R=1, KBT=1)
+T = 10     
+h = 0.001     
+
+v = 200        
+
+a = 1     
+B_R = 1
+KBT = 1     
+B_T = 3 / 4     
+
+mu = a
+
+sim = afbm(alpha=alpha, T=T, h=h, v=v, mu=mu, B_T=B_T, B_R=B_R, KBT=KBT)
 
 r, phi = sim.solve()
 k = 20
 n = len(r[0])
 x = np.interp(np.arange(n * k-k), np.arange(n) * k, r[0])
 y = np.interp(np.arange(n * k-k), np.arange(n) * k, r[1])
-t = np.arange(0, len(x))
+t = np.linspace(0, T, len(x))
+# tstart = 0.1
+# tend = 3
+# tstart_index = int(T / h * tstart)
+# tend_index = int(T / h * tend)
+# print(f"tstart_index: {tstart_index}, tend_index: {tend_index}")
+# x = x[tstart_index:tend_index]
+# y = y[tstart_index:tend_index]
+# t = t[tstart_index:tend_index]
+# 
+import seaborn as sns
+cmap = sns.color_palette("Paired", as_cmap=True)
 fig, ax = plt.subplots(figsize=(7, 7))
-ax.scatter(x, y, s = 10, c = t, cmap='Greens')
-ax.set_xlabel("x")
-ax.set_ylabel("y")
+sc = ax.scatter(x, y, s = 8, c = t, cmap=cmap)
+ax.set_xlabel(r"$x/a$")
+ax.set_ylabel(r"$y/a$")
 ax.axis("equal")
-# fig.savefig(f'plots/traj_a{alpha}_T{T}_h{h}_v{v}_mu{mu}.png', dpi=200)
-fig.savefig(f'plots/traj.png', dpi=200)
+cbar = fig.colorbar(sc, ax=ax)
+cbar.set_label(r"$t/a$")
+fig.savefig(f'plots/traj_a{alpha}_T{T}_h{h}_v{v}_mu{mu}.png', dpi=200)
+# fig.savefig(f'plots/traj{v}.png', dpi=200)
+# fig.savefig(f'plots/traj.png', dpi=200)
