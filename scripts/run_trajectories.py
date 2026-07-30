61f92c393a913b5acb840c81488dffb48e962a45
nicagali <monyconte@live.it>
2026-07-30 11:20:06 +0200

    run and save trajectories in batches

--- scripts/run_trajectories.py
+++ scripts/run_trajectories.py
@@ -1,5 +1,5 @@
 import numpy as np
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, cpu_count
 import sys
 sys.path.append("src/")
 from afbm import afbm
@@ -22,12 +22,19 @@ def ensemble_traj(alpha, T, h, v, mu, B_T, B_R, KBT, realizations, n_jobs=-1):
 
     seeds = np.random.randint(0, 2**32 - 1, size=realizations)
 
-    results = Parallel(n_jobs=n_jobs)(delayed(traj)(alpha, T, h, v, mu, B_T, B_R, KBT, seed) for seed in tqdm(seeds))
+    batch_size = cpu_count() if n_jobs==-1 else n_jobs
 
-    pos_traj = np.array([r[0] for r in results])
-    ang_traj = np.array([r[1] for r in results])
+    for batch_id, start in enumerate(range(0, realizations, batch_size)):
 
-    return t, pos_traj, ang_traj
+        end = min(start + batch_size, realizations) #if realizations not multiple of batch size, the last batch will have a different end
+        results = Parallel(n_jobs=n_jobs)(delayed(traj)(alpha, T, h, v, mu, B_T, B_R, KBT, seed) for seed in tqdm(seeds[start:end], desc=f"Batch {batch_id + 1}"))
+
+        r_batch = np.array([r[0] for r in results])
+        phi_batch = np.array([r[1] for r in results])
+
+        np.savez(f"data/traj_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}_batch{batch_id:02d}.npz", t=t, r_batch=r_batch, phi_batch=phi_batch)
+
+    return 
 
 if __name__ == "__main__":
 
@@ -37,10 +44,9 @@ if __name__ == "__main__":
     v = float(sys.argv[4])
     mu = float(sys.argv[5])
     realizations = int(sys.argv[6])
+    n_jobs = int(sys.argv[7])
     B_T = 1
     B_R = 1
     KBT = 1
-    n_jobs=-1 #use all cores
 
-    t, traj_r, traj_phi = ensemble_traj(alpha=alpha, T=T, h=h, v=v, mu=mu, B_T=B_T, B_R=B_R, KBT=KBT, realizations=realizations, n_jobs=-1)
-    np.savez(f"data/traj_a{alpha}_T{T}_h{h}_v{v}_mu{mu}_r{realizations}.npz", t=t, traj_r=traj_r, traj_phi=traj_phi)
+    ensemble_traj(alpha=alpha, T=T, h=h, v=v, mu=mu, B_T=B_T, B_R=B_R, KBT=KBT, realizations=realizations, n_jobs=n_jobs)
